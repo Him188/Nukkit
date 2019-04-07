e494f30d3566185ed067e1fc2891d7d7cd4d1e99
Him188 <Him188@mamoe.net>
2019-04-07 15:49:50 +0800

    updated

--- src/main/java/cn/nukkit/event/player/PlayerArmorChangeEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerArmorChangeEvent.java
@@ -1,9 +1,15 @@
 package cn.nukkit.event.player;
 
 import cn.nukkit.Player;
+import cn.nukkit.event.HandlerList;
 import cn.nukkit.item.Item;
 
 public class PlayerArmorChangeEvent extends PlayerEvent{
+    private static final HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
 
     private Item from;
     private Item to;
